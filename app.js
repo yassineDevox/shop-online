fafea4d9fc54a338ac30288e22c65431f9d3d1bf
Yassine Rassy <yassine.rassy1@gmail.com>
2020-10-12 21:43:33 +0100

    fix errors

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use((req,res)=>{
+    res.json({
+        message:"Im Here Yassine !!"
+    })
+})
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const app = require('./app');
-const { env } = require('process');
 
-app.set('port',env.process.PORT || 3000);
+    app.set('port',process.env.PORT || 3000)
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+    server.listen(process.env.PORT || 3000)
+
