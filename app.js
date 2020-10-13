a922fa8fcfe44b84c41484b7ee14b3dcbc3b079e
Yassine Rassy <yassine.rassy1@gmail.com>
2020-10-13 21:01:00 +0100

    add post api for adding articles

--- app.js
+++ app.js
@@ -15,28 +15,6 @@ app.use((req, res, next) => {
       message: 'Objet créé !'
     });
   });
-  
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Yassine',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',   
-        title: 'Fanni',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
 
 
 module.exports = app;
