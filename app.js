9b290ef0f90de884199ac4e9109b7bcd514da3fd
Yassine Rassy <yassine.rassy1@gmail.com>
2020-10-13 20:34:36 +0100

    add a get api for the list of the product api/stuff

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res,next)=>{
 
-    console.log("réponse 1");
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-})
+  });
 
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Yassine',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',   
+        title: 'Fanni',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
-
-app.use((req,res,next)=>{
-    res.status(201);
-    next();
-})
-
-
-app.use((req,res)=>{
-    res.end("réponse 2");
-})
 
 module.exports = app;
